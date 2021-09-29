a50f08388140d65186172477aaef091c549358f7
Agalvf <alejito20401@hotmail.com>
2021-09-29 15:15:21 -0500

    Se agregó Vue en el archivo productos.html

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+    el: "#app",
+    data:{  
+
+        productosArray : [
+            {
+                img:"../img/producto.png",
+                nombreProducto:"Taladro percutor",
+                descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
+                precio: "$ 200.000"
+            },
+            {
+                img:"../img/producto.png",
+                nombreProducto:"Taladro percutor",
+                descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
+                precio: "$ 200.000"
+            },
+            {
+                img:"../img/producto.png",
+                nombreProducto:"Taladro percutor",
+                descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
+                precio: "$ 200.000"
+            },
+            {
+                img:"../img/producto.png",
+                nombreProducto:"Taladro percutor",
+                descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
+                precio: "$ 200.000"
+            },
+            {
+                img:"../img/producto.png",
+                nombreProducto:"Taladro percutor",
+                descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
+                precio: "$ 200.000"
+            }     
+        ]
+    },
+    methods:{
+
+    }
+})
