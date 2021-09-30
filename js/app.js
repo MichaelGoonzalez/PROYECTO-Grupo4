4ecf9cad07c27df2bfeb0754af8af176e854f6fd
Mich Labs <88068139+MichaelGoonzalez@users.noreply.github.com>
2021-09-29 22:06:39 -0500

    entorno listo para mongo
    se empieza a crear la base de datos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 /*express creation server*/
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 //import routes 
-
+const indexRoutes = require('../routes/index');
 
 //settings
 app.set('port', process.env.PORT || 3000);
-app.set
+app.set('html', path.join(__dirname, 'html'));
+app.set('html engine', 'ejs');
 
 //middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+//routes
+app.use('/', indexRoutes);
 
 
 //starting Server
