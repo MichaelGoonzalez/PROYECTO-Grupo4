d3792a6d449a25c79b2a6bccf9ecb782c9553472
Mich Labs <88068139+MichaelGoonzalez@users.noreply.github.com>
2021-10-03 20:03:40 -0500

    base de datos
    integracion

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://localhost/database')
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err));
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//midlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/tasks', require('./routes/tasks'));
+
+//static files
+app.use(express.static(__dirname + '/public'));
+
+//server is listening
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
