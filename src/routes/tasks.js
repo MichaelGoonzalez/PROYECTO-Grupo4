92c15974a25749ffcb03986ca0ca894eebb49270
Mich Labs <88068139+MichaelGoonzalez@users.noreply.github.com>
2021-10-07 16:55:07 -0500

    servidor

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('API Task is goes here')
+const Task = require('../models/Task');
+
+//get data
+router.get('/', async(req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+//send data
+router.post('/', async (req, res) =>{
+  const task = new Task(req.body);
+  await task.save();
+  res.json({
+    status: 'Task Saved'
+  });
+});
+
+//update data
+router.put('/:id', async(req, res) => {
+  await Task.findByIdAndUpdate(req.params.id, req.body);
+  res.json({
+    status: 'Task Updated'
+  });
+});
+
+//delete data
+router.delete('/:id', async (req, res) => {
+  await Task.finByIdAndRemove(req.params.id);
+  res.json({
+    status: 'Task Deleted'
+  })
 });
 
 module.exports = router;
